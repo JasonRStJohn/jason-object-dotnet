1de223529d8f79b1709d04d88ccfb8c49ae9dd28
Developer <dev@example.com>
2026-07-07 15:50:55 +0000

    feat: add MudBlazor, switch to global InteractiveServer, remove Bootstrap

--- src/MeDotNet/Program.cs
+++ src/MeDotNet/Program.cs
@@ -5,6 +5,7 @@ using MeDotNet.Data;
 using MeDotNet.Models;
 using MeDotNet.Services.Auth;
 using MeDotNet.Services.Posts;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddRazorPages();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
